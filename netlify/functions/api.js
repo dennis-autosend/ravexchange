40dd9a81701a8af703a6713f2b782ddf2e48800e
dennis-autosend <dennis@autosend.com>
2024-10-09 08:46:55 -0700

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// Root path handler
 app.get('/', (req, res) => {
-  console.log('Root path accessed');
-  res.json({ message: 'Welcome to the ravExchange API' });
+  res.json({ message: 'Hello from the API!' });
 });
 
-// Wrap the Express app with serverless
 const handler = serverless(app);
 
-// Export the handler function
 exports.handler = async (event, context) => {
-  console.log('Function executed!', event.path);
   return await handler(event, context);
 };
+
