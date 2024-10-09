985a3e8c6af970e1e080713590300de684c69013
dennis-autosend <dennis@autosend.com>
2024-10-09 08:58:39 -0700

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,20 @@
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello, World!" })
-  };
+  const { path } = event;
+
+  if (path === '/.netlify/functions/api/hello') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello, World!" })
+    };
+  } else if (path === '/.netlify/functions/api/goodbye') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Goodbye, World!" })
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Not Found" })
+    };
+  }
 };
