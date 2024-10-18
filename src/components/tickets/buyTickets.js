da0e4b65dcb58748f79800a1cd6d0e03f69ce9cd
tokyoundergroundbeatzmassive <122478522+tokyoundergroundbeatzmassive@users.noreply.github.com>
2024-10-18 12:24:27 +0900

    Not showing Seller's phone number

--- src/components/tickets/buyTickets.js
+++ src/components/tickets/buyTickets.js
@@ -13,7 +13,7 @@ export const handleBuyTicket = async (post, isLoggedIn, setIsVerificationOpen, u
         });
         const data = await response.json();
         if (data.success) {
-            alert(`SMS sent to seller at ${post.mobileNumber}. Seller will contact you to complete the purchase.`);
+            alert(`SMS sent to seller. Seller will contact you to complete the purchase.`);
         } else {
             alert(`Failed to send SMS. Error: ${data.error}`);
         }
